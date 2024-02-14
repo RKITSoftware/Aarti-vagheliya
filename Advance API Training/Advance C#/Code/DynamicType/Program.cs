35cac90cab15c40e5e0953a57d441f34e324ca8d
Aarti-vagheliya <artivagheliya@gmail.com>
2024-02-14 12:41:08 +0530

    Dynamic Type

--- Advance API Training/Advance C#/Code/DynamicType/Program.cs
+++ Advance API Training/Advance C#/Code/DynamicType/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Dynamic;
 
 namespace DynamicType
 {
-    internal class Program
+    /// <summary>
+    /// Demonstrates the usage of dynamic types in C#.
+    /// </summary>
+    public class Program
     {
         static void Main(string[] args)
         {
+            // Creating a dynamic variable
+            dynamic dynamicVariable;
+
+            // Assigning different types of values to the dynamic variable
+
+            dynamicVariable = 10;
+            Console.WriteLine($"Dynamic variable value: {dynamicVariable}\n \"Type of this variable => {dynamicVariable.GetType()}\" ");
+            Console.WriteLine();
+
+            dynamicVariable = "Hello, dynamic!";
+            Console.WriteLine($"Dynamic variable value: {dynamicVariable} \n \"Type of this variable => {dynamicVariable.GetType()}\" ");
+            Console.WriteLine();
+
+            dynamicVariable = new { Name = "John", Age = 30 };
+            Console.WriteLine($"Dynamic variable value: Name: {dynamicVariable.Name}, Age: {dynamicVariable.Age} ");
+            Console.WriteLine();
+
+            // Accessing members dynamically
+            dynamic dynamicObject = new ExpandoObject();
+            dynamicObject.Name = "Alice";
+            dynamicObject.Age = 25;
+            Console.WriteLine($"Name: {dynamicObject.Name}, Age: {dynamicObject.Age}");
+            Console.WriteLine();
+
+            // Calling methods dynamically
+            dynamic calculator = new Calculator();
+            Console.WriteLine($"Addition result: {calculator.Add(5, 3)}");
+            Console.WriteLine();
+
+            // Using dynamic in a loop
+            foreach (dynamic item in new dynamic[] { 1, "two", 3.0 })
+            {
+                Console.WriteLine($"Dynamic item: {item}");
+            }
+        }
+
+        // Sample class for dynamic method invocation
+        public class Calculator
+        {
+            public int Add(int a, int b) => a + b;
         }
     }
 }
+
