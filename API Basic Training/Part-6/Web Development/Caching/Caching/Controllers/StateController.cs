72bd32b7f6b4b8c2399adad28712725d3e48d2ca
Aarti-vagheliya <artivagheliya@gmail.com>
2024-03-15 16:53:24 +0530

    Caching Done

---
+++ API Basic Training/Part-6/Web Development/Caching/Caching/Controllers/StateController.cs
@@ -0,0 +1,51 @@
+﻿using Caching.Data;
+using Caching.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Http;
+
+namespace Caching.Controllers
+{
+    /// <summary>
+    /// Controller for state which is handel method calling
+    /// </summary>
+    public class StateController : ApiController
+    {
+        private Cache _objCache = new Cache();
+        private static Stopwatch _stopwatch;
+
+        public StateController()
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        [HttpGet]
+        [Route("api/State/FetchStateData")]
+        public IHttpActionResult FetchStateData()
+        {
+            
+            List<State> statedata;
+            if (_objCache["Cached data"] != null)
+            {
+                 statedata = (List<State>)_objCache.Get("Cached data");
+            }
+            else
+            {
+                // Retrieves state data from the data source.
+                statedata = StateData.RetriveState();
+
+                _objCache.Insert("Cached data", statedata, null, DateTime.Now.AddMinutes(20), TimeSpan.Zero);
+            }
+            _stopwatch.Stop();
+            long responseTime = _stopwatch.ElapsedTicks;
+            HttpContext.Current.Response.AddHeader("Response-time", responseTime.ToString());
+            //HttpContext.Current.Response.AddHeader("Cache-control",);
+
+            // Returns an HTTP response with the retrieved state data.
+            return Ok(statedata);
+        }
+    }
+}
---
+++ API Basic Training/Part-6/Web Development/Caching/Caching/Models/State.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Caching.Models
+{
+    public class State
+    {
+        public int StateId { get; set; }
+        public string StateName { get;  set; }
+        public string StateCode { get; set; }
+    }
+}
