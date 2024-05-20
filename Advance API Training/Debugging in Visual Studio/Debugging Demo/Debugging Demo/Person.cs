c84daf64a87cff71f508db95810a31eaaafb8dbf
Aarti-vagheliya <artivagheliya@gmail.com>
2024-05-20 18:23:02 +0530

    Changes applied

---
+++ Advance API Training/Debugging in Visual Studio/Debugging Demo/Debugging Demo/Person.cs
@@ -0,0 +1,71 @@
+﻿using System.Data;
+
+namespace Debugging_Demo
+{
+    // Nested Address class
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+
+
+    /// <summary>
+    /// Represents a person with a name, age, and additional details stored in a DataTable.
+    /// </summary>
+    public class Person
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the name of the person.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the age of the person.
+        /// </summary>
+        public int Age { get; set; }
+
+        public Address HomeAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the details of the person stored in a DataTable.
+        /// </summary>
+        public DataTable Details { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Person"/> class.
+        /// </summary>
+        public Person()
+        {
+            Details = new DataTable();
+            Details.Columns.Add("Key", typeof(string));
+            Details.Columns.Add("Value", typeof(string));
+            HomeAddress = new Address();
+        }
+
+        #endregion
+
+        #region Public Method
+
+        /// <summary>
+        /// Adds a detail about the person to the DataTable.
+        /// </summary>
+        /// <param name="key">The key of the detail.</param>
+        /// <param name="value">The value of the detail.</param>
+        public void AddDetail(string key, string value)
+        {
+            DataRow row = Details.NewRow();
+            row["Key"] = key;
+            row["Value"] = value;
+            Details.Rows.Add(row);
+        }
+
+        #endregion
+    }
+}
--- Advance API Training/Debugging in Visual Studio/Debugging Demo/Debugging Demo/Program.cs
+++ Advance API Training/Debugging in Visual Studio/Debugging Demo/Debugging Demo/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Debugging_Demo
 {
@@ -17,7 +13,6 @@ namespace Debugging_Demo
         /// </summary>
         /// <param name="num">The number for which to calculate the factorial.</param>
         /// <returns>The factorial of the input number.</returns>
-  
         public static int Factorial(int num)
         {
             int fact = 1;
@@ -29,13 +24,40 @@ namespace Debugging_Demo
             return fact;
         }
 
-
+        /// <summary>
+        /// main method of the application.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+#if DEBUG
+            Console.WriteLine("Debugging is enabled");
+#else
+            Console.WriteLine("This is a Release build");
+#endif
+
+
             //Calling method Factorial
             Console.Write("Enter number for find factorial: ");
             int num = int.Parse(Console.ReadLine());
             Console.WriteLine("factorial = {0}", Factorial(num));
+
+            Console.WriteLine("------------------------------------------");
+
+            // Create a Person object
+            Person person = new Person
+            {
+                Name = "John Doe",
+                Age = 30,
+                HomeAddress = new Address { Street = "123 Main St", City = "New York" }
+            };
+
+            // Add details to the DataTable
+            person.AddDetail("Occupation", "Software Developer");
+            person.AddDetail("City", "New York");
+
+            // Breakpoint here to inspect the person object and its DataTable
+            Console.WriteLine("Debugging Demo - Set a breakpoint on this line.");
         }
     }
 }
